601c99b6186c243ae29b9a530545434317624399
Jace Chan <jacetchan@gmail.com>
2018-06-06 16:07:28 -0700

    Render basic App component

---
+++ client/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+var App = (props) => (
+  <div>Hello</div>
+);
+
+export default App;
--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-ReactDOM.render(<div>Test</div>, document.getElementById('review-module'));
+ReactDOM.render(<App />, document.getElementById('review-module'));
