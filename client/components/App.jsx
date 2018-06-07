d24a0c419750ce88200215cfd10dda7c3b96b0b1
Jace Chan <jacetchan@gmail.com>
2018-06-06 17:07:00 -0700

    Create Overview component with rendered data

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,17 +1,42 @@
 import React from 'react';
+import axios from 'axios';
+import Overview from './Overview.jsx';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-
+      stats: {}
     };
+  }
+
+  componentDidMount() {
+    console.log('component MOUNTED');
+    this.getOverview();
+    // this.getReviews();
+  }
+
+  getOverview() {
+    axios.get('/reviews/listingId/2') // TODO manually checks listing 2 atm
+      .then((response) => {
+        this.setState({stats: response.data[0]});
+      })
+      .catch((err) => {
+        // TODO
+      });
+  }
+
+  getReviews() {
+
   }
 
   render() {
     return (
-      <div>Hello World!</div>
+      <div>
+        Hello World!
+        <Overview stats={this.state.stats} />
+      </div>
     );
   }
 }
---
+++ client/components/Overview.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Overview = (props) => {
+
+  const accuracy = props.stats.avg_accuracy;
+  const checkin = props.stats.avg_checkin;
+  const cleanliness = props.stats.avg_cleanliness;
+  const communication = props.stats.avg_communication;
+  const location = props.stats.avg_location;
+  const value = props.stats.avg_value;
+  
+  const totalReviews = props.stats.total_reviews;
+  const overview = (accuracy + checkin + cleanliness + communication + 
+    location + value) / (6);
+
+  return (
+    <div>
+      {totalReviews} Reviews [{overview} <br/>
+      Accuracy [{accuracy}] <br/>
+      Location [{checkin}] <br/>
+      Communication [{cleanliness}] <br/>
+      Check-in [{communication}] <br/>
+      Cleanliness [{location}] <br/>
+      Values [{value}] <br/>
+    </div>
+  );
+};
+
+export default Overview;
