84c9140c7b9a41406b8e95d5b27bef5444dab4be
Jace Chan <jacetchan@gmail.com>
2018-06-15 19:52:16 -0700

    Create Dockerfile and update db config

--- database/config.js
+++ database/config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  user: 'root',
-  database: 'project_nomad_reviews'
+  host: 'hrsf96-reviews-db.c41wfzxfgrjb.us-west-1.rds.amazonaws.com',
+  database: 'reviews',
+  user: 'reviewdbroot',
+  password: 'password1234',
+  port: 3306,
+  timeout: 60000,
 };
